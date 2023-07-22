4ea8cbe323b2b2237922d5ae80b8ba95aa18f712
ravenstudios <ravenstudiosrob@yahoo.com>
2023-07-22 01:54:50 -0500

    Working on movment and turning corners.

--- player.py
+++ player.py
@@ -10,8 +10,9 @@ class Player(sprite.Sprite):
     def __init__(self, x, y, color):
         super().__init__(x, y, color)
         self.speed = 3
-        self.x_speed = self.speed
-        self.y_speed = 0
+        # self.move_vec[0] = self.speed
+        # self.move_vec[1] = 0
+        self.move_vec = (1, 0)
 
     def update(self, map):
         self.collision(map)
@@ -19,20 +20,25 @@ class Player(sprite.Sprite):
         self.move(map)
 
     def move(self, map):
-        collided_objects = pygame.sprite.spritecollide(self, map, False)
+        temp_sprite = pygame.sprite.Sprite()
+        temp_vec = self.move_vec * self.speed
+        temp_sprite.rect = self.rect.move(temp_vec[0], temp_vec[1])
+        # temp_sprite.rect.topl
+
+        collided_objects = pygame.sprite.spritecollide(temp_sprite, map, False)
 
         if collided_objects:
             for co in collided_objects:
-                if self.x_speed:
-                    self.rect.right = co.rect.left if self.x_speed > 0 else co.rect.left + co.rect.width * 2
-                    self.x_speed = 0
-                if self.y_speed:
-                    self.rect.bottom = co.rect.top if self.y_speed > 0 else co.rect.top + co.rect.height * 2
-                    self.y_speed = 0
+                if self.move_vec[0]:
+                    self.rect.right = co.rect.left if self.move_vec[0] > 0 else co.rect.left + co.rect.width * 2
+
+                if self.move_vec[1]:
+                    self.rect.bottom = co.rect.top if self.move_vec[1] > 0 else co.rect.top + co.rect.height * 2
+
 
         else:
-            self.rect.left += self.x_speed
-            self.rect.top += self.y_speed
+            self.rect.left += self.move_vec[0]
+            self.rect.top += self.move_vec[1]
 
 
     def collision(self, map):
@@ -45,7 +51,7 @@ class Player(sprite.Sprite):
                 #
                 #
                 # if type(co) == portal.Portal:
-                #     self.rect.left = BLOCK_SIZE if self.x_speed > 0 else GAME_WIDTH - (BLOCK_SIZE * 2)
+                #     self.rect.left = BLOCK_SIZE if self.move_vec[0] > 0 else GAME_WIDTH - (BLOCK_SIZE * 2)
 
 
 
@@ -59,17 +65,13 @@ class Player(sprite.Sprite):
         keys = pygame.key.get_pressed()
 
         if (keys[pygame.K_w] or keys[pygame.K_UP]):
-            self.x_speed = 0
-            self.y_speed = -self.speed
+            self.move_vec = (0, -1)
 
         if (keys[pygame.K_d] or keys[pygame.K_RIGHT]):
-            self.x_speed = self.speed
-            self.y_speed = 0
+            self.move_vec = (1, 0)
 
         if (keys[pygame.K_s] or keys[pygame.K_DOWN]):
-            self.x_speed = 0
-            self.y_speed = self.speed
+            self.move_vec = (0, 1)
 
         if (keys[pygame.K_a] or keys[pygame.K_LEFT]):
-            self.x_speed = -self.speed
-            self.y_speed = 0
+            self.move_vec = (-1, 0)
